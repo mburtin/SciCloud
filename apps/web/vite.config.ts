87c9a668672da0275f2a3cf4127a1f81199edd5f
BURTIN Michael <michael.burtin@univ-poitiers.fr>
2025-07-21 13:54:27 +0200

    Frontend: setup ShadCN & TailwindCSS

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
+import path from 'node:path'
+import tailwindcss from '@tailwindcss/vite'
 import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    tailwindcss()
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  server: {
+    port: 3000,
+    host: true,
+  },
 })
