547573382d7679c42ac97e4d898bdd4354febeb4
BURTIN Michael <michael.burtin@univ-poitiers.fr>
2025-07-29 19:38:29 +0200

    Frontend: add Vue DevTools

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,13 +1,15 @@
 import path from 'node:path'
 import tailwindcss from '@tailwindcss/vite'
 import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
 import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    tailwindcss()
+    tailwindcss(),
+    vueDevTools()
   ],
   resolve: {
     alias: {
